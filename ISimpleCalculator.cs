f0a4433afa11144080f3d4e47f759b374acdc291
wibse <wibse@DESKTOP-7CLD65E>
2023-07-25 17:37:33 +0100

    Add project files.

---
+++ ISimpleCalculator.cs
@@ -0,0 +1,8 @@
+﻿namespace CalculatorTest
+{
+    public interface ISimpleCalculator
+    {
+        int Add(int start, int amount);
+        int Subtract(int start, int amount);
+    }
+}
---
+++ SimpleCalculator.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CalculatorTest
+{
+    public class SimpleCalculator : ISimpleCalculator
+    {
+        // Note that overflows on the value of int simply roll round to the opposite end of the int value range 
+        // without throwing an exception (e.g int.MaxValue + 1 takes you back to int.MinValue)
+        public int Add(int start, int amount) => start + amount;
+
+        public int Subtract(int start, int amount) => start - amount;
+    }
+}
